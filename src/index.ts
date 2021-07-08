82c8393973591256e1246d25a7b11e94f932e0c4
William Chong <me@williamchong.cloud>
2021-07-08 17:02:15 +0800

    🍱 Add proto and src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as iscnid from './iscn/iscnid';
+export * as param from './iscn/params';
+export * as query from './iscn/query';
+export * as store from './iscn/store';
+export * as tx from './iscn/tx';
